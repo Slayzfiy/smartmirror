2675712c2c8241630a8dc78406a24c7b3cfb6797
Fabulus <fabio@kreisern.eu>
2022-04-02 15:58:27 +0200

    - modified start script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 git pull
 kill -9 $(lsof -t -i:1080)
-nohup python3 app.py
+nohup python3 app.py &
 DISPLAY=:0 chromium-browser "$(hostname -I | awk '{print $1}'):1080" --start-fullscreen --disable-session-crashed-bubble
